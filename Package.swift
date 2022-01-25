7dfc0bfd9a0954d5b40378872aafdfd90684f408
Sunnyyoung <iSunnyyoung@gmail.com>
2022-01-25 22:40:47 +0800

    Update dependencies

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version:5.3
+// swift-tools-version:5.5
 
 import PackageDescription
 
@@ -17,11 +17,11 @@ let package = Package(
     ],
     dependencies: [
         .package(url: "https://github.com/Alamofire/Alamofire", from: "4.9.1"),
-        .package(url: "https://github.com/mxcl/PromiseKit", from: "6.15.3"),
-        .package(url: "https://github.com/apple/swift-argument-parser", from: "0.5.0")
+        .package(url: "https://github.com/mxcl/PromiseKit", from: "6.16.2"),
+        .package(url: "https://github.com/apple/swift-argument-parser", from: "1.0.2")
     ],
     targets: [
-        .target(
+        .executableTarget(
             name: "WeChatTweak-CLI",
             dependencies: [
                 "Alamofire",
